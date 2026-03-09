2e0d2c9274b902ab07fae993b75f867962c4f680
Narmatha345 <narmathasambath34@gmail.com>
2026-03-09 17:28:08 +0530

    Change import path for calculateLumpSumRollingXirr

--- src/utils/calculations/lumpSumRollingXirr/worker.ts
+++ src/utils/calculations/lumpSumRollingXirr/worker.ts
@@ -4,7 +4,7 @@ self.onmessage = async function (e) {
 
   const { navDataList, years, allocations, investmentAmount } = e.data;
 
-  const module = await import('./calculateLumpSumRollingXirr');
+  const module = await import('./index');
 
   const result = module.calculateLumpSumRollingXirr(
     navDataList,
